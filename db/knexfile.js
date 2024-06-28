a8ab3ef4623141b46c4a3dd2b0b99a777c2793a3
sachinelavarasan <sachinelavarasan321@gmail.com>
2024-06-28 21:45:15 +0530

    local postgres db setup completed

--- db/knexfile.js
+++ db/knexfile.js
@@ -1,15 +1,15 @@
 const knexOptions = {
   client: 'pg',
-  connection: process.env.DATABASE_URL,
-  // connection: {
-  //   database: process.env.DB_DATABASE,
-  //   host: process.env.DB_HOST,
-  //   password: process.env.DB_PASSWORD,
-  //   // timezone: 'utc',
-  //   user: process.env.DB_USER,
-  // },
-  
-  debug: true,
+  // connection: process.env.DATABASE_URL,
+  connection: {
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    password: process.env.DB_PASSWORD,
+    // timezone: 'utc',
+    user: process.env.DB_USER,
+  },
+
+  debug: process.env.NODE_ENV === 'development',
   // migrations: {
   //   directory: path.join(__dirname, './migrations'),
   //   tableName: 'migrations',
